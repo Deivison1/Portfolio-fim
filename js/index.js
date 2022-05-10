508109babe78917f3a9ed9ca9f13694261f24ef9
Deivison1 <santosdeivison480@gmail.com>
2022-05-10 13:39:36 -0300

    atualizando animações

--- js/index.js
+++ js/index.js
@@ -4,3 +4,18 @@ checkbox.addEventListener('change', () => {
     document.body.classList.toggle('light-mode')
 })
 
+const sr = ScrollReveal({
+
+    origin: 'top',
+    distance: '68px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal('.container-conteudo',{interval: 50})
+sr.reveal('.conteudo-img', {delay: 500, interval: 50})
+sr.reveal('.paragrato-conteudo', {delay: 600, interval: 50})
+sr.reveal('.caixa-project', {delay: 700, interval: 50})
+sr.reveal('.nbvki', {delay: 800, origin: 'left', interval: 20})
+
